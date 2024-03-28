53b99eabc7c20ad1f5936705bc58060f51c802f1
unknown <sakshipadamwar2002@gmail.com>
2024-03-28 10:30:09 +0530

    added skills section

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,6 +1,87 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { BsInfoCircleFill } from "react-icons/bs";
+import html from "../../images/html.png";
+import css from "../../images/css.png";
+import js from "../../images/Javascript.png";
+import  react from "../../images/react.png";
+import "./style.scss";
+
+const data = [
+    {
+       tech:"HTML",
+       tag:"frontend",
+    },
+    {
+        tech:"CSS",
+        tag:"frontend",
+     },
+     {
+        tech:"JavaScript",
+        tag:"frontend",
+     },
+     {
+        tech:"React/Redux",
+        tag:"frontend",
+     },
+     {
+        tech:"Node js",
+        tag:"backend"
+     },
+     {
+        tech:"Express js",
+        tag:"backend"
+     },
+     {
+        tech:"MongoDB",
+        tag:"backend"
+     },
+     {
+        tech:"MySQL",
+        tag:"backend"
+     },
+     {
+        tech:"C",
+        tag:"languages"
+     },
+     {
+        tech:"C++",
+        tag:"languages"
+     },
+     
+     {
+        tech:"Java",
+        tag:"languages"
+     },
+     {
+        tech:"Python",
+        tag:"languages"
+     },
+     {
+         tech:"Github",
+         tag:"tool"
+     },
+     {
+         tech:"Netlify",
+         tag:"tool"
+     },
+     {
+         tech:"Android Studio",
+         tag:"tool"
+     },
+     {
+        tech:"postman",
+        tag:"tool"
+     }
+
+     
+];
+//this will give you the another array 
+const frontendArray = data.filter(item=>item.tag.includes("frontend"));
+const backendArray = data.filter(item=>item.tag.includes("backend"));
+const languages = data.filter(item=>item.tag.includes("languages"));
+const tools = data.filter(item=>item.tag.includes("tool"));
+
 const Skills = () => {
     return (
         <section id="about" className="about">
@@ -9,6 +90,60 @@ const Skills = () => {
                 headerText="My Skills"
                 icon={<BsInfoCircleFill size={40} />}
             ></PageHeaderContent>
+
+            <div className="skill">
+                <div className="skill__container">
+                    <h1> &nbsp; FRONT END &nbsp;</h1>
+                    <div className="skill__container__innerContent">
+                    {
+                        frontendArray.map((item)=>{
+                            return(
+                               <p>{item.tech}</p>
+                            )
+                           
+                        })
+                    }
+                    </div>
+                    
+                </div>
+                <div className="skill__container">
+                    <h1> &nbsp; BACK END&nbsp;</h1>
+                    <div className="skill__container__innerContent">
+                        {
+                            backendArray.map((item)=>{
+                                return(
+                                    <p>{item.tech}</p>
+                                )
+                            })
+                        }
+                    </div>
+                   
+                </div>
+                <div className="skill__container">
+                    <h1> &nbsp; LANGUAGES&nbsp;</h1>
+                    <div className="skill__container__innerContent">
+                        {
+                            languages.map(item=>{
+                                return(
+                                    <p>{item.tech}</p>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <div className="skill__container">
+                    <h1> &nbsp; TOOLS&nbsp;</h1>
+                    <div className="skill__container__innerContent">
+                        {
+                            tools.map(item=>{
+                                return(
+                                    <p>{item.tech}</p>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
